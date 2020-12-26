9e4b23f5ecbad7581bc6163a36fa50670a3d4c32
Dmitry Karpov <dem0schase@gmail.com>
2020-12-27 02:43:00 +0300

    html template, list view for marketplace

--- ecom/parser_app/urls.py
+++ ecom/parser_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from .views import MarketPlaceListView
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('categories/', views.categories, name='categories'),
-    path('urls/', views.urls, name='urls'),
-    path('urls/<int:url_id>/', views.detail_url, name='url_details'),
+    # path('', views.index, name='index'),
+    path('', MarketPlaceListView.as_view(), name='parser-home'),
+    # path('categories/', views.categories, name='categories'),
+    # path('urls/', views.urls, name='urls'),
+    # path('urls/<int:url_id>/', views.detail_url, name='url_details'),
 ]
 
